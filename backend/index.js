bd72577774c3d1a2d4d44f6873d02d6d9e5ece43
harish-bhati-ui <harishbhati.bhati@gmail.com>
2026-01-08 16:41:38 +0530

    backend index file change for upload api

--- backend/index.js
+++ backend/index.js
@@ -148,6 +148,6 @@ app.post("/api/ask", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0",() => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
